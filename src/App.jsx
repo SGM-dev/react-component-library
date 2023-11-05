bc39af8e973e227cdea2ea2a8e82553230edc687
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-05 16:27:50 +0530

    Initialize Avatar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import "./App.css";
 import Button from "./components/Button/Button";
+import Avatar from "./components/Avatar/Avatar";
 
 function App() {
   return (
     <main>
-      <Button size="sm" variant="warning" onClick={() => console.log("...logging in")}>
-        Click Here
-      </Button>
+      <section>
+        {" "}
+        <Button
+          size="sm"
+          variant="warning"
+          onClick={() => console.log("...logging in")}
+        >
+          Click Here
+        </Button>
+      </section>
+      <section>
+        <Avatar src="./sam.jpeg" alt="Sam George Mathew" />
+        <br />
+        <Avatar>BZ</Avatar>
+        <br />
+        <Avatar />
+      </section>
     </main>
   );
 }
---
+++ src/components/Avatar/Avatar.jsx
@@ -0,0 +1,9 @@
+import {IoPersonSharp} from "react-icons/io5"
+import "./Avatar.css"
+
+
+export default function Avatar(props) {
+    return (
+        <h2>Avatar Component</h2>
+    )
+}
