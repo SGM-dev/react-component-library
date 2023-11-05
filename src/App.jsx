fe632dac78a71ab8bdc4d4b1026a6f5cce6b19b9
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-05 15:57:06 +0530

    remove react-icons as its not used anymore here

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import Button from "./components/Button/Button";
-import { FaMoneyBill } from "react-icons/fa";
 
 function App() {
   return (
     <main>
       <Button size="sm" variant="warning" onClick={() => console.log("...logging in")}>
-        <FaMoneyBill />
         Click Here
       </Button>
     </main>
