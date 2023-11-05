6a3aa942ab37cf88d65e9ce940d0f5e22f9d55e0
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-05 16:55:20 +0530

    set random color if image is not provided

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -3,9 +3,13 @@ import "./Avatar.css";
 import classNames from "classnames";
 
 export default function Avatar({ children, src, ...rest }) {
+  const colors = ["navy", "pink", "red", "blue", "green"];
+
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
   const avatarClass = !src && (children ? "avatar-letters" : "avatar-icon");
 
-  const allClasses = classNames("avatar", avatarClass);
+  const allClasses = classNames("avatar", avatarClass, !src && randomColor);
 
   const content = src ? (
     <img src={src} {...rest}></img>
