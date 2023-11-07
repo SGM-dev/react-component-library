0799e54065e1921062c4768e42082eaa3a428f8d
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-07 14:38:47 +0530

    Remove comments

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -5,17 +5,6 @@ import { useState } from "react";
 import "./Menu.css";
 
 export default function Menu({ children }) {
-  /**
-   * Note: leave the div className="menu" here and render
-   * the children inside that div. Notice this component will become
-   * significantly simpler by doing so 💡
-   *
-   * Also, notice our state will be broken after we make
-   * these changes - that's okay! We'll fix it soon. In the meantime,
-   * leave the useState() call and toggle() definitions alone. Your
-   * new version won't be using them, but we'll come back to them
-   * later.
-   */
   const [open, setOpen] = useState(true);
 
   function toggle() {
