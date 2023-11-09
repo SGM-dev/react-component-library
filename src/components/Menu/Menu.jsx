dc24dbcb9abefee930869713dbbab4d5ddd90671
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-09 12:52:28 +0530

    move toggle function to Context and use destructuring

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -7,14 +7,14 @@ import "./Menu.css";
 const MenuContext = createContext();
 
 export default function Menu({ children }) {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
 
   function toggle() {
     setOpen((prevOpen) => !prevOpen);
   }
 
   return (
-    <MenuContext.Provider value={false}>
+    <MenuContext.Provider value={{ open, toggle }}>
       <div className="menu">
         {React.Children.map(children, (child) => {
           return React.cloneElement(child, {
--- src/components/Menu/MenuButton.jsx
+++ src/components/Menu/MenuButton.jsx
@@ -1,5 +1,9 @@
+import { useContext } from "react";
 import Button from "../Button/Button";
+import { MenuContext } from "./Menu";
 
-export default function MenuButton({ children, toggle }) {
+export default function MenuButton({ children }) {
+  const { toggle } = useContext(MenuContext);
+
   return <Button onClick={toggle}>{children}</Button>;
 }
