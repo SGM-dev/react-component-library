cb8a5093313173da4f85facd5f4966573c663d66
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-12 08:30:32 +0530

    Revert "setup toggle component"
    This reverts commit 672db08ff0b24f33ba6324672161377151e8bc85.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import "./App.css";
-import Star from "./components/Toggle/Star";
+import Button from "./components/Button/Button";
+import Avatar from "./components/Avatar/Avatar";
+import Menu from "./components/Menu/index";
 
 function App() {
   const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"];
 
   return (
     <main>
-      <Star />
+      <Menu>
+        <Menu.Button>Sports</Menu.Button>
+        <Menu.Dropdown>
+          {sports.map((sport) => (
+            <Menu.Item>{sport}</Menu.Item>
+          ))}
+        </Menu.Dropdown>
+      </Menu>
     </main>
   );
 }
