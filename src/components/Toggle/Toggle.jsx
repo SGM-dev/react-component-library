34562c355eb53d20c6f6edafe04ebd8138c272f2
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-12 22:30:37 +0530

    fix typo at Toggle()

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -11,7 +11,7 @@ export default function Toggle({ children }) {
 
   return (
     <ToggleContext.Provider value={{ on, toggle }}>
-      children
+      {children}
     </ToggleContext.Provider>
   );
 }
