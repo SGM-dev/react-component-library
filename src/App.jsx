39b94758203780fc6f5d44fc4da7c7ec6a418875
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-12 23:49:26 +0530

    move Star into seperate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import "./App.css";
 import Toggle from "./components/Toggle/index";
 import Menu from "./components/Menu/index";
-import { BsStar, BsStarFill } from "react-icons/bs";
+import Star from "./components/Star";
 
 function App() {
   const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"];
 
   return (
     <main>
-      <Toggle>
-        <Toggle.Button>
-          <Toggle.On>
-            <BsStarFill className="star filled" />
-          </Toggle.On>
-          <Toggle.Off>
-            <BsStar className="star " />
-          </Toggle.Off>
-        </Toggle.Button>
-      </Toggle>
+      <Star />
       <br />
       <Toggle>
         <Menu>
