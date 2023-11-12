e0fe520c9601d47de21c5544f3eb4775811cb1c6
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-12 23:43:18 +0530

    use Toggle with Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Toggle from "./components/Toggle/index";
+import Menu from "./components/Menu/index";
 import { BsStar, BsStarFill } from "react-icons/bs";
 
 function App() {
@@ -16,6 +17,21 @@ function App() {
             <BsStar className="star " />
           </Toggle.Off>
         </Toggle.Button>
+      </Toggle>
+      <br />
+      <Toggle>
+        <Menu>
+          <Toggle.Button>
+            <Menu.Button>Sports</Menu.Button>
+          </Toggle.Button>
+          <Toggle.On>
+            <Menu.Dropdown>
+              {sports.map((sport) => (
+                <Menu.Item key={sport}>{sport}</Menu.Item>
+              ))}
+            </Menu.Dropdown>
+          </Toggle.On>
+        </Menu>
       </Toggle>
     </main>
   );
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,30 +1,5 @@
-import MenuButton from "./MenuButton";
-import MenuDropdown from "./MenuDropdown";
-import React, { useState, createContext } from "react";
-
 import "./Menu.css";
 
-const MenuContext = createContext();
-
 export default function Menu({ children }) {
-  const [open, setOpen] = useState(false);
-
-  function toggle() {
-    setOpen((prevOpen) => !prevOpen);
-  }
-
-  return (
-    <MenuContext.Provider value={{ open, toggle }}>
-      <div className="menu">
-        {React.Children.map(children, (child) => {
-          return React.cloneElement(child, {
-            open,
-            toggle,
-          });
-        })}
-      </div>
-    </MenuContext.Provider>
-  );
+  return <div className="menu">{children}</div>;
 }
-
-export { MenuContext };
