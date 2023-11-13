739db7f36c20c05c58779fbdef70df0ac64f0f0d
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-13 11:19:50 +0530

    use render props in Toggle.Display to render box conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import "./App.css";
-import Menu from "./components/Menu/index";
-import Star from "./components/Star";
+import Toggle from "./components/Toggle/index";
 
 function App() {
   return (
     <main>
-      <Star />
-
-      <br />
-
-      <Menu onOpen={() => console.log("Menu toggled!")}>
-        <Menu.Button>Menu</Menu.Button>
-        <Menu.Dropdown>
-          <Menu.Item>Home</Menu.Item>
-          <Menu.Item>About</Menu.Item>
-          <Menu.Item>Contact</Menu.Item>
-          <Menu.Item>Blog</Menu.Item>
-        </Menu.Dropdown>
-      </Menu>
+      <Toggle>
+        <Toggle.Button>
+          <Toggle.Display>
+            {(on) => <div className={`box ${on ? "filled" : ""}`}></div>}
+          </Toggle.Display>
+        </Toggle.Button>
+      </Toggle>
     </main>
   );
 }
+
+// <Star />
+
+// <br />
+
+// <Menu>
+//   <Menu.Button>Menu</Menu.Button>
+//   <Menu.Dropdown>
+//     <Menu.Item>Home</Menu.Item>
+//     <Menu.Item>About</Menu.Item>
+//     <Menu.Item>Contact</Menu.Item>
+//     <Menu.Item>Blog</Menu.Item>
+//   </Menu.Dropdown>
+// </Menu>
 
 export default App;
