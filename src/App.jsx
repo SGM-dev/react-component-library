ebfcab4620506dbb3bdafd784620fc63fae97635
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-13 06:32:55 +0530

    add Menu onOpen event

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,21 @@ import Menu from "./components/Menu/index";
 import Star from "./components/Star";
 
 function App() {
-  const sports = ["Tennis", "Pickleball", "Racquetball", "Squash"];
-
   return (
     <main>
-      <Star onChange={() => console.log("Toggled!")} />
+      <Star onChange={() => {}} />
+
       <br />
+
+      <Menu onOpen={() => console.log("Menu Toggled!")}>
+        <Menu.Button>Menu</Menu.Button>
+        <Menu.Dropdown>
+          <Menu.Item>Home</Menu.Item>
+          <Menu.Item>About</Menu.Item>
+          <Menu.Item>Contact</Menu.Item>
+          <Menu.Item>Blog</Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
     </main>
   );
 }
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,9 @@
 import "./Menu.css";
 import Toggle from "../Toggle/index";
 
-export default function Menu({ children }) {
+export default function Menu({ children, onOpen }) {
   return (
-    <Toggle>
+    <Toggle onToggle={onOpen}>
       <div className="menu">{children}</div>
     </Toggle>
   );
