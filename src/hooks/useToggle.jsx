8996b6bc7729bcba4eed4a25423c427eac720ab1
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-13 19:02:54 +0530

    add initial value as parameter to useToggle

--- src/hooks/useToggle.jsx
+++ src/hooks/useToggle.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
-export default function useToggle() {
-  const [on, setOn] = useState(false);
+export default function useToggle(initialValue = false) {
+  const [on, setOn] = useState(initialValue);
 
   function toggle() {
     setOn((prevOn) => !prevOn);
