664b30ad64ff1b3e3758ce892765dd8912a2c2b7
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-13 18:42:07 +0530

    create and export MenuContext

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,7 +1,16 @@
 import "./Menu.css";
 import useToggle from "../../hooks/useToggle";
+import { createContext } from "react";
+
+const MenuContext = createContext();
 
 export default function Menu({ children, onOpen }) {
   const [open, toggleOpen] = useToggle();
-  return <div className="menu">{children}</div>;
+  return (
+    <MenuContext.Provider value={{ open, toggleOpen }}>
+      <div className="menu">{children}</div>
+    </MenuContext.Provider>
+  );
 }
+
+export { MenuContext };
