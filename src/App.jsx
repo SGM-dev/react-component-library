7f3e9841ebcbf01d4b62b750de8f4572f19fdf39
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-11-13 16:42:50 +0530

    add comments for readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
 import "./App.css";
 import Toggle from "./components/Toggle/index";
+import Star from "./components/Star";
 
 function App() {
   return (
     <main>
-      <Toggle onToggle={() => console.log("works")}>
-        <Toggle.Button>
-          <Toggle.Display>
-            {(on) => <div className={`box ${on ? "filled" : ""}`}></div>}
-          </Toggle.Display>
-        </Toggle.Button>
-      </Toggle>
+      <Star />
     </main>
   );
 }
 
+// Using Toggle.Display for the render props
+// <Toggle onToggle={() => {console.log("Toggled")}}>
+//   <Toggle.Button>
+//     <Toggle.Display>
+//       {(on) => {
+//         return <div className={`box ${on ? "filled" : ""}`}></div>
+//       }}
+//     </Toggle.Display>
+//   </Toggle.Button>
+// </Toggle>
+// Example using Toggle.On and Toggle.Off to conditionally render items
+// <Toggle.On>
+//   <div className="box filled"></div>
+// </Toggle.On>
+// <Toggle.Off>
+//   <div className="box"></div>
+// </Toggle.Off>
+
+// Simple Star component that uses the Toggle component internally
 // <Star />
 
 // <br />
+
+// Menu Component using Toggle component internally to manage
+// state and context
 
 // <Menu>
 //   <Menu.Button>Menu</Menu.Button>
