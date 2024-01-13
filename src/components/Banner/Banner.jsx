d98e68d6ca084d447c6d00a0a667b4f74e383926
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-01-13 11:49:28 +0530

    add global styling to banner component

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,5 +1,12 @@
 import "./Banner.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function Banner({ children }) {
-  return <div className="banner">{children}</div>;
+  return (
+    <div className="banner success">
+      <FontAwesomeIcon icon={faCircleCheck} />
+      <div className="banner-content">{children}</div>
+    </div>
+  );
 }
