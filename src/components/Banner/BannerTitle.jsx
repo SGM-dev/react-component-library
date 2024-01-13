d4bf45bafec607604ba271d7a593ddbf13c1d42d
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-01-13 12:02:47 +0530

    remove unused imports and rules

--- src/components/Banner/BannerTitle.jsx
+++ src/components/Banner/BannerTitle.jsx
@@ -1,6 +1,4 @@
 import "./Banner.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function BannerTitle({ children }) {
   return <h2 className="banner-title">{children}</h2>;
