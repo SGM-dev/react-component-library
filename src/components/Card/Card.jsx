0d2a1b2ae926646f237d4fa87d5ba880435b2fef
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2024-01-15 14:53:35 +0530

    update Card to run functions and add styles on hover

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,12 +1,30 @@
 import "./Card.css";
-import PropTypes from "prop-types";
 import classNames from "classnames";
+import { useRef } from "react";
+import PropTypes from "prop-types";
+import useHover from "../../hooks/useHook";
 
-export default function Card({ children, className, ...rest }) {
-  const allClasses = classNames("card-container", className);
+export default function Card({
+  children,
+  className,
+  onHover,
+  onLeave,
+  ...rest
+}) {
+  const elementRef = useRef();
+
+  const isHovered = useHover({
+    ref: elementRef,
+    onHover,
+    onLeave,
+  });
+
+  const hoverClass = isHovered ? "shadow" : "";
+
+  const allClasses = classNames("card-container", hoverClass, className);
 
   return (
-    <div className={allClasses} {...rest}>
+    <div className={allClasses} {...rest} ref={elementRef}>
       {children}
     </div>
   );
